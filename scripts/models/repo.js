381a57d902585491dbc5e73ede7742c9a4e43be8
Gloria Guy <totallygloria@gmail.com>
2016-10-05 13:33:13 -0700

    Reconfigured the $.get AJAX call to use the $.ajax method instead. Added the Authentication headers to the server ajax call.

--- scripts/models/repo.js
+++ scripts/models/repo.js
@@ -4,12 +4,18 @@
   reposObj.allRepos = [];
 
   reposObj.requestRepos = function(callback) {
-    // TODO: refactor this request into an $.ajax call
-    $.get('/github/users/codefellows-portland-301d6/repos' +
-          '?per_page=10&sort=updated')
-          .done(function(data) {
-            reposObj.allRepos = data;
-          }).done(callback);
+    // DONE: TODO: refactor this request into an $.ajax call
+    $.ajax({
+      url: '/github/users/codefellows-portland-301d6/repos' +
+      '?per_page=10&sort=updated',
+      success: function (data) {
+        reposObj.allRepos = data;
+        callback();
+      },
+      error: function (xhr, settings, error) {
+        console.log('Server returned an error: ', error);
+      }
+    });
   };
 
   reposObj.withTheAttribute = function(attr) {
